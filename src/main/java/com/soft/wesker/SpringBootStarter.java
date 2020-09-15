f78ffdecebae8cd89c3de975b62a476cdaca03d9
wesker <1045561395@qq.com>
2020-09-15 21:27:01 +0800

    v1.3

--- src/main/java/com/soft/wesker/SpringBootStarter.java
+++ src/main/java/com/soft/wesker/SpringBootStarter.java
@@ -3,13 +3,14 @@ package com.soft.wesker;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 @MapperScan("com.soft.wesker.dao")
 public class SpringBootStarter{
     public static void main(String[] args) {
         //启动SpringBoot工程
-       SpringApplication.run(SpringBootStarter.class,args);
+        ConfigurableApplicationContext run = SpringApplication.run(SpringBootStarter.class, args);
     }
 
 }
